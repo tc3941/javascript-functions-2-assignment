05caeb5acc63feac606002d6c68e055388a50de2
Teddy Conyers Jr <tc3941@rit.edu>
2023-06-27 21:05:59 -0400

    Add changes as directed in the exercises

--- script.js
+++ script.js
@@ -4,64 +4,64 @@
 // - Then uses the .filter() method to return a filtered array of books containing that genre.
 
 //Write filterByGenre(books, genre) below here:
-
+function filterByGenre(books, genre) {
+  return books.filter((book) => book.genre === genre)
+}
 
 const books = [
   {
-    title: "The Da Vinci Code",
-    author: "Dan Brown",
-    genre: "mystery",
+    title: 'The Da Vinci Code',
+    author: 'Dan Brown',
+    genre: 'mystery',
   },
   {
-    title: "Pride and Prejudice",
-    author: "Jane Austen",
-    genre: "romance",
+    title: 'Pride and Prejudice',
+    author: 'Jane Austen',
+    genre: 'romance',
   },
   {
     title: "The Hitchhiker's Guide to the Galaxy",
-    author: "Douglas Adams",
-    genre: "sci-fi",
+    author: 'Douglas Adams',
+    genre: 'sci-fi',
   },
   {
-    title: "The Girl with the Dragon Tattoo",
-    author: "Stieg Larsson",
-    genre: "mystery",
+    title: 'The Girl with the Dragon Tattoo',
+    author: 'Stieg Larsson',
+    genre: 'mystery',
   },
   {
-    title: "To Kill a Mockingbird",
-    author: "Harper Lee",
-    genre: "romance",
+    title: 'To Kill a Mockingbird',
+    author: 'Harper Lee',
+    genre: 'romance',
   },
   {
     title: "Ender's Game",
-    author: "Orson Scott Card",
-    genre: "sci-fi",
+    author: 'Orson Scott Card',
+    genre: 'sci-fi',
   },
   {
-    title: "The Silence of the Lambs",
-    author: "Thomas Harris",
-    genre: "mystery",
+    title: 'The Silence of the Lambs',
+    author: 'Thomas Harris',
+    genre: 'mystery',
   },
   {
-    title: "Gone with the Wind",
-    author: "Margaret Mitchell",
-    genre: "romance",
+    title: 'Gone with the Wind',
+    author: 'Margaret Mitchell',
+    genre: 'romance',
   },
   {
-    title: "Dune",
-    author: "Frank Herbert",
-    genre: "sci-fi",
+    title: 'Dune',
+    author: 'Frank Herbert',
+    genre: 'sci-fi',
   },
   {
-    title: "The Big Sleep",
-    author: "Raymond Chandler",
-    genre: "mystery",
+    title: 'The Big Sleep',
+    author: 'Raymond Chandler',
+    genre: 'mystery',
   },
   {
-    title: "Jane Eyre",
-    author: "Charlotte Bronte",
-    genre: "romance",
+    title: 'Jane Eyre',
+    author: 'Charlotte Bronte',
+    genre: 'romance',
   },
-];
-
-
+]
